16d9e619f12a10a267e8693343a2ab89c5b803f1
ErikMille <miller.erik.1998@gmail>
2019-07-19 14:42:37 +0200

    reeeee

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts/app')
 @section('content')
 
-<div id="user" data-user="{{$user}}" data-tutor="{{$user->tutor?$user->tutor:''}}" data-subjects="{{$user->tutor->subject}}" data-names="{{$subject_names}}" data-edit="{{(Auth::user()&&Auth::user()->id==$user->id)?true:false}}"></div>
+<div id="user" data-user="{{$user}}" data-tutor="{{$user->tutor?$user->tutor:''}}" data-subjects="{{$user->tutor?$user->tutor->subject:''}}" data-names="{{$subject_names}}" data-edit="{{(Auth::user()&&Auth::user()->id==$user->id)?true:false}}"></div>
 
 <?php 
     // dd($user->tutor->subject);
